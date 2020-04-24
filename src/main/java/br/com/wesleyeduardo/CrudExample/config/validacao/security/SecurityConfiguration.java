835850a0a110924a059d92850b05a7f6c905c22e
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-04-24 10:34:27 -0300

    formLogin

--- src/main/java/br/com/wesleyeduardo/CrudExample/config/validacao/security/SecurityConfiguration.java
+++ src/main/java/br/com/wesleyeduardo/CrudExample/config/validacao/security/SecurityConfiguration.java
@@ -2,17 +2,39 @@ package br.com.wesleyeduardo.CrudExample.config.validacao.security;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+
+import javax.naming.AuthenticationNotSupportedException;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //COnfigurações de Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
 
+    //Configurações de Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
 
-
-
+    //Configurações de Recursos estaticos (js,css,imagens, etc..)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }

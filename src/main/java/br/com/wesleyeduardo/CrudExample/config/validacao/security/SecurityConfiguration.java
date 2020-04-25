a55c7061abf9ebaa165de500b74d0ce71b7991a4
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-04-25 14:36:47 -0300

    DocumentationwithSwagger

--- src/main/java/br/com/wesleyeduardo/CrudExample/config/validacao/security/SecurityConfiguration.java
+++ src/main/java/br/com/wesleyeduardo/CrudExample/config/validacao/security/SecurityConfiguration.java
@@ -49,6 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/topicos").permitAll()
                 .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
                 .antMatchers(HttpMethod.POST,"/auth").permitAll()
+                .antMatchers(HttpMethod.GET,"/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -57,6 +58,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 
     }
 }
